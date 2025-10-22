284921070d19e32b169e0d5dc8252abb7e326aa0
Dmytro <77779585+artswanted@users.noreply.github.com>
2025-10-22 09:40:35 +0300

    effects

---
+++ sfreedom_dev/Data/5_Effect/4_Pet/Common/ef_dark_cloud_00/ef_dark_cloud_Anim.h
@@ -0,0 +1,16 @@
+// This file was automatically generated. It contains definitions for all the
+// animations stored in the associated KFM file. Include this file in your
+// final application to easily refer to animation sequences.
+
+#ifndef EF_DARK_CLOUD_ANIM_H__
+#define EF_DARK_CLOUD_ANIM_H__
+
+namespace ef_dark_cloud_Anim
+{
+    enum
+    {
+        SKILL_00                = 1000000
+    };
+}
+
+#endif  // #ifndef EF_DARK_CLOUD_ANIM_H__
